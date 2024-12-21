28ad6e311134d46e737b59c9c8048d1f391ba627
MFaisal125 <mfaisal0125470@gmail.com>
2024-12-21 08:36:46 +0500

    p

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Socials from "../components/Socials";
 const Header = () => {
   return (
-    <header className="absolute w-full z-10 flex items-center px-16 xl:px-0 xl:ml-[-50px] xl:h-[80px]">
+    <header className="absolute w-full z-10 flex items-center px-1 xl:px-0 xl:ml-[-50px] xl:h-[80px]">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           <Link href={"/"}>
